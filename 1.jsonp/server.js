2a80cadcb1ccc31a43daf1599f4d65d3653eb673
fallred <fallred@sina.com>
2019-03-07 20:16:54 +0800

    jsonp server

--- 1.jsonp/server.js
+++ 1.jsonp/server.js
@@ -3,6 +3,7 @@ let app = express();
 app.get('/say', function(req, res){
     let {wd, cb} = req.query;
     console.log(wd);
+    // 取出客户端请求参数，解析出回调函数，执行。
     res.end(`${cb}('我不带你')`);
 });
 app.listen(3000);
