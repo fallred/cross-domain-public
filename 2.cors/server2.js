8904ca5209be2226098700ef7eaecc69bf2daffb
fallred <fallred@sina.com>
2019-03-07 22:45:26 +0800

    cors

--- 2.cors/server2.js
+++ 2.cors/server2.js
@@ -3,12 +3,21 @@ let app = express();
 let whiteList = ['http://localhost:3000'];
 app.use(function(req, res, next){
     let origin = req.headers.origin;
-    // 设置哪个源可以访问我
     if (whiteList.includes(origin)) {
+        // 设置哪个源可以访问我
         res.setHeader('Access-Control-Allow-Origin', origin);
+        // 允许携带哪个头访问我
         res.setHeader('Access-Control-Allow-Headers', 'name');
+        // 允许哪个方法可以访问我
         res.setHeader('Access-Control-Allow-Methods', 'PUT');
-        res.setHeader('Access-Control-Max-Age', 6000);
+        // 允许携带cookie
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        // 预检存活时间
+        res.setHeader('Access-Control-Max-Age', 6);
+        // 允许前端获取哪个头
+        res.setHeader('Access-Control-Expose-Headers', 'name');
+        // 允许返回的头
+        // res.setHeader('Access-Control-Expose-Headers', 'name');
         if (req.method === 'OPTIONS') {
             // OPTIONS请求不做任何处理
             res.end();
@@ -18,6 +27,7 @@ app.use(function(req, res, next){
 });
 app.put('/getData', function(req, res){
     console.log(req.headers);
+    res.setHeader('name','jw');
     res.end('我不爱你');
 });
 app.get('/getData', function(req, res){
